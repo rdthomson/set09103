80ca8512840c2e4874cdf241a39c3cbefe42a23c
Ryan Thomson <rthomson97@me.com>
2017-09-21 11:48:41 +0100

    completed 6 examples from Learn Python the Hard Way

---
+++ src/ex3.py
@@ -0,0 +1,40 @@
+# Prints text
+print "I will now count my chickens:"
+
+# Prints text, then does addition and division
+print "Hens", 25.0 + 30.0 / 6.0
+
+# Prints text, then does subtraction, multiplication and returns the remainder of the division
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+#Prints text
+print "I will now count the eggs:"
+
+#Prints equasion
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Prints question
+print "Is it true that 3 + 2 < 5 -7?"
+#Tells if 5 is less than -2
+print 3 + 2 < 5 - 7
+
+#Prints question and answer
+print "What is 3 + 2?", 3 + 2
+
+#Prints question and answer
+print "What is 5 - 7?", 5 - 7
+
+#Prints text
+print "Oh, that's why it's Flase."
+
+#Prints text
+print "How about some more?"
+
+#Prints question and answer
+print "Is it greater?", 5 > -2
+
+#Prints question and answer
+print "Is it greater or equal?", 5 >= -2
+
+#Prints question and answer
+print "Is it less ro equal?", 5 <= -2
