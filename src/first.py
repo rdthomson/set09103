e60a13b1dd7e0ecff697913a8cf6f7ecad2a4809
Ryan Thomson <rthomson97@me.com>
2017-09-21 10:34:12 +0100

    Created hello.txt and got first.py working

--- src/first.py
+++ src/first.py
@@ -1,8 +1,8 @@
 from flask import Flask
-app - Flask(__name__)
+app = Flask(__name__)
 @app.route("/")
 def hello():
-	return  "Help Napier"
+	return  "Hello Napier"
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=9135)
