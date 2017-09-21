6316b6aea58adf9618d6cabd273f294d2b3e0584
Ryan Thomson <rthomson97@me.com>
2017-09-21 16:00:50 +0100

    Started week 3 examples from workbook

--- src/workbook/first.py
+++ src/workbook/first.py
@@ -1,8 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
-	return  "Hello Napier"
+	return "Hello World!"
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=9135)
+	app.run(host='0.0.0.0')
