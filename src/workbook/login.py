46b79e9a42dda4f24d9cedbffd4b00cb385cd785
Ryan Thomson <rthomson97@me.com>
2017-09-23 19:54:00 +0100

    Started chapter 4 of the workbook

--- src/workbook/login.py
+++ src/workbook/login.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, abort
 app = Flask (__name__)
 
 @app.route("/private")
@@ -10,6 +10,14 @@ def private():
 @app.route('/login')
 def login():
 	return "Now we would get username & text"
+
+@app.route('/force404')
+def force404():
+	abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return "Couldn't find the page you requested.", 404
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
